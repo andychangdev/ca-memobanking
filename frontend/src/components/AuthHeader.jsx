6ee3613c7eff30ef8a839f1bf9d55703fb9f9bb0
andychangdev <andychang.qld@gmail.com>
2024-07-27 19:54:44 +1000

    style: Modified header of auth pages

--- frontend/src/components/AuthHeader.jsx
+++ frontend/src/components/AuthHeader.jsx
@@ -1,17 +1,17 @@
 import { NavLink, Link } from "react-router-dom";
-import logo from "../assets/react.svg"
+import logo from "../../public/logo.svg";
 
 export function AuthHeader() {
   return (
-    <header className="content-grid">
-    <nav className="breakout">
-      <div class="brandmark">
-        <NavLink to={"/"} className="brandmark__container">
-          <img src={logo} className="brandmark__logo" alt="Memobanking Logo" />
-          <p className="brandmark_text">Memobanking</p>
-        </NavLink>
-      </div>
-    </nav>
+    <header>
+      <nav className="auth-header">
+        <div class="brandmark">
+          <NavLink to={"/"} className="brandmark__container">
+            <img src={logo} className="brandmark__logo" alt="Memobanking Logo"/>
+            <p className="brandmark__text">Memobanking</p>
+          </NavLink>
+        </div>
+      </nav>
     </header>
   );
 }
--- frontend/src/components/BaseHeader.jsx
+++ frontend/src/components/BaseHeader.jsx
@@ -1,22 +1,22 @@
 import { NavLink, Link } from "react-router-dom";
-import logo from "../assets/react.svg"
+import logo from "../../public/logo.svg"
 
 export function BaseHeader() {
   return (
     <header className="content-grid">
-    <nav className="breakout">
-      <div class="brandmark">
-        <NavLink to={"/"} className="brandmark__container">
-          <img src={logo} className="brandmark__logo" alt="Memobanking Logo" />
-          <p className="brandmark_text">Memobanking</p>
-        </NavLink>
-      </div>
+      <nav className="base-header breakout">
+        <div class="brandmark">
+          <NavLink to={"/"} className="brandmark__container">
+            <img src={logo} className="brandmark__logo" alt="Memobanking Logo" />
+            <p  className="brandmark__text">Memobanking</p>
+          </NavLink>
+        </div>
       
-      <div className="button">
-      <Link to="/login" className="button--login">Login</Link>
-      <Link to="/signup" className="button--signup"> Sign Up</Link>
-      </div>
-    </nav>
+        <div className="button">
+          <Link to="/login" className="button--login">Login</Link>
+          <Link to="/signup" className="button--signup"> Sign Up</Link>
+        </div>
+      </nav>
     </header>
   );
 }
