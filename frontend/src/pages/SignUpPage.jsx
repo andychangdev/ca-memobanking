925465a3e2a6ee622f803c3abb17d666e4c2558a
andychangdev <andychang.qld@gmail.com>
2024-07-27 15:23:53 +1000

    chore: Added placeholder text to pages

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,8 @@
 export function SignUpPage() {
     return (
-        <h1>SignUp Page</h1>
+        <section className="content-grid">
+        <h1>Sign Up Page</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore quasi blanditiis quibusdam hic neque. Earum sed itaque nemo, maxime harum maiores aut dignissimos fugiat dolores adipisci fuga sequi similique reprehenderit?</p>
+    </section>
     )
 }
