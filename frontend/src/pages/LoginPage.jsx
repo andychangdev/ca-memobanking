3f771d2c1739215382af2aee7f55a54f4a8dada3
andychangdev <andychang.qld@gmail.com>
2024-07-29 23:01:16 +1000

    feat: Added auth redirect links and styles to make app more responsive

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,21 +1,25 @@
-export function LoginPage() {
-    return (
-      <section className="auth-form content-grid">
-        <div className="auth-form__content">
-          <h1>Welcome Back!</h1>
-          <p>Login to your Memobank.</p>
-        <form>
-          <div className="auth-form__user-input">
-          <label htmlFor="username">Username</label>
-          <input id="username" placeholder="Enter your username" type="text" />
+import { Link } from "react-router-dom";
 
-          <label htmlFor="password">Password</label>
-          <input id="password" placeholder="Enter your password" type="password" />
+export function LoginPage() {
+  return (
+    <section className="auth-page content-grid">
+      <div className="auth-page__content">
+        <h1>Welcome Back!</h1>
+        <p>Login to your Memobank.</p>
+        <form className="auth-form">
+          <div className="auth-form__user-input">
+            <label htmlFor="username">Username</label>
+            <input id="username" placeholder="Enter your username" type="text"/>
+
+            <label htmlFor="password">Password</label>
+            <input id="password" placeholder="Enter your password" type="password"/>
           </div>
           <button className="auth-form__button">Continue</button>
         </form>
-        </div>
-      </section>
-    );
-  }
-  
+        <p className="auth-form__redirect">Not registered yet? {" "}
+          <Link to="/signup"> Create an account</Link>
+        </p>
+      </div>
+    </section>
+  );
+}
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,25 +1,30 @@
+import { Link } from "react-router-dom";
+
 export function SignUpPage() {
   return (
-    <section className="auth-form content-grid">
-      <div className="auth-form__content">
+    <section className="auth-page content-grid">
+      <div className="auth-page__content">
         <h1>Join Memobanking</h1>
         <p>Sign up for free!</p>
-      <form>
-        <div className="auth-form__user-input">
-          <label htmlFor="username">Username</label>
-          <input id="username" placeholder="Choose a unique username" type="text" />
+        <form className="auth-form">
+          <div className="auth-form__user-input">
+            <label htmlFor="username">Username</label>
+            <input id="username" placeholder="Choose a unique username" type="text" />
 
-          <label htmlFor="firstname">Firstname</label>
-          <input id="firstname" placeholder="Enter your first name" type="text" />
+            <label htmlFor="firstname">Firstname</label>
+            <input id="firstname" placeholder="Enter your first name" type="text" />
 
-          <label htmlFor="lastname">Lastname</label>
-          <input id="lastname" placeholder="Enter your last name" type="text" />
+            <label htmlFor="lastname">Lastname</label>
+            <input id="lastname" placeholder="Enter your last name" type="text" />
 
-          <label htmlFor="password">Password</label>
-          <input id="password" placeholder="Create a secure password" type="password" />
-        </div>
-        <button className="auth-form__button">Create account</button>
-      </form>
+            <label htmlFor="password">Password</label>
+            <input id="password" placeholder="Create a secure password" type="password" />
+          </div>
+          <button className="auth-form__button">Create account</button>
+        </form>
+        <p className="auth-form__redirect">Already have an account? {" "}
+            <Link to="/login"> Log In</Link>
+        </p>
       </div>
     </section>
   );
