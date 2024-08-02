2629b4482e5855f0334ff0de637eb1674d946a8b
andychangdev <andychang.qld@gmail.com>
2024-08-02 23:30:08 +1000

    feat: Added state management and input validation to input fields in Login Page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,40 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function LoginPage() {
+
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+  
+
+  const handleInputChange = (event) => {
+    const { id, value } = event.target;
+    switch (id) {
+      case "username":
+        setUsername(value);
+        if (!value) {
+          setUsernameError("Username is required");
+        } else {
+          setUsernameError("")
+        }
+        break;
+      case "password":
+        setPassword(value);
+        if (!value) {
+          setPasswordError("Password is required");
+        } else {
+          setPasswordError("")
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+
   return (
     <section className="auth-page content-grid">
       <div className="auth-page__content">
@@ -9,10 +43,12 @@ export function LoginPage() {
         <form className="auth-form">
           <div className="auth-form__user-input">
             <label htmlFor="username">Username</label>
-            <input id="username" placeholder="Enter your username" type="text"/>
+            <input id="username" className={`${usernameError ? "auth-form__input--error" : ""}`} placeholder="Enter your username" type="text" value={username} onChange={handleInputChange}/>
+            {usernameError ? <p className="auth-form__error">{usernameError}</p> : null}
 
             <label htmlFor="password">Password</label>
-            <input id="password" placeholder="Enter your password" type="password"/>
+            <input id="password" className={`${passwordError ? "auth-form__input--error" : ""}`} placeholder="Enter your password" type="password" value={password} onChange={handleInputChange}/>
+            {passwordError ? <p className="auth-form__error">{passwordError}</p> : null}
           </div>
           <button className="auth-form__button">Continue</button>
         </form>
