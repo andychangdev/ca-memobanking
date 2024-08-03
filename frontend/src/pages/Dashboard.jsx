cab204b97c158ea3dce0341ac84fdd6bd5d64b7d
andychangdev <andychang.qld@gmail.com>
2024-08-03 19:50:27 +1000

    feat: Implemented basic responsive layout

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,9 +4,17 @@ export function Dashboard() {
     return (
         <main>
             <DashboardHeader/>
-            <section className="content-grid">
-                <h1>Dashboard</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore quasi blanditiis quibusdam hic neque. Earum sed itaque nemo, maxime harum maiores aut dignissimos fugiat dolores adipisci fuga sequi similique reprehenderit?</p>
+            <section className="dashboard content-grid">
+                <div className="dashboard__container">
+                    <div className="dashboard__content">
+                        <h1>Dashboard</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore quasi blanditiis quibusdam hic neque. Earum sed itaque nemo, maxime harum maiores dignissimos fugiat dolores adipisci fuga sequi similique reprehenderit?</p>
+                    </div>
+                    <div className="dashboard__content">
+                        <h1>Dashboard</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore quasi blanditiis quibusdam hic neque. Earum sed itaque nemo, maxime harum maiores aut dignissimos fugiat dolores adipisci fuga sequi similique reprehenderit?</p>
+                    </div>
+                </div>
             </section>
         </main>
     )
