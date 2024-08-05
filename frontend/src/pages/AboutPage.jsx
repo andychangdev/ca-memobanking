ff9a73618ffe7af8dea69c9a98206ad5caf7e101
andychangdev <andychang.qld@gmail.com>
2024-08-05 20:51:20 +1000

    feat: Added images to about page and contact page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,24 +1,35 @@
 import { BaseHeader, Footer } from "../components";
+import appJoinImg from "../assets/images/app_about.svg";
 
 export function AboutPage() {
-    return (
-        <main>
-            <BaseHeader/>
-            <section className="content-grid">
-                <div className="card">
-                    <h2>About Us</h2>
-                    <p>At Memobanking, we understand the importance of mental health in today's world. Our mission is to provide young people with a practical and engaging way to track their emotions, promoting self-awareness and emotional regulation.</p>
-                </div>
-                <div className="card">
-                    <h2>Our Vision</h2>
-                    <p>We believe in a world where mental health is prioritized and understood. By offering personalised insights and the ability to share experiences, Memobanking not only aids individual users but also fosters a broader conversation about mental health, promoting understanding and support within the community.</p>
-                </div>
-                <div className="card">
-                    <h2>Our Team</h2>
-                    <p>Our team is passionate about mental health and technology. We have combined our expertise to create a platform that is both user-friendly and impactful. Together, we are dedicated to helping you navigate your emotional landscape and achieve mental wellness.</p>
-                </div>
-            </section>
-            <Footer/>
-        </main>
-    )
+  return (
+    <main>
+      <BaseHeader btnColour="btn-purple"/>
+      <section className="content-grid">
+        <div className="grid-container">
+          <img className="grid-container__image" src={appJoinImg}></img>
+          <div>
+            <div className="hp--card">
+              <h1>About Us</h1>
+              <p>
+                At Memobanking, we understand the importance of mental health in
+                today's world. Our mission is to provide young people with a
+                practical and engaging way to track their emotions, promoting
+                self-awareness and emotional regulation.
+              </p>
+              <h2>Our Vision</h2>
+              <p>
+                We believe in a world where mental health is prioritized and
+                understood. By offering personalised insights and the ability to
+                share experiences, Memobanking not only aids individual users
+                but also fosters a broader conversation about mental health,
+                promoting understanding and support within the community.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </main>
+  );
 }
