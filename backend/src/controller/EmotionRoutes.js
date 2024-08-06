2869e7d2d026d03a14fc71e945d29d501083c7d6
andychangdev <andychang.qld@gmail.com>
2024-08-06 22:31:09 +1000

    feat: Added CRUD operation to delete an emotion entry

--- backend/src/controller/EmotionRoutes.js
+++ backend/src/controller/EmotionRoutes.js
@@ -54,5 +54,28 @@ router.get("/get-all-emotions", verifyToken, async (request, response) => {
     }
 })
 
+// Delete Emotion Entry
+router.delete("/delete-emotion/:entryId", verifyToken, async (request, response) => {
+    try {
+        const userId = request.user.id;
+        const entryId = request.params.entryId;
+
+        const entry = await Emotion.findOne({ _id: entryId, userId: userId });
+
+        if (!entry) {
+            return response.status(404).json({ message: "Emotion entry not found"})
+        }
+
+        await Emotion.deleteOne({ _id: entryId, userId: userId });
+
+        return response.json({
+            message: "Emotion entry deleted successfully"
+        })
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({ message: "Server Error", error: error.message});
+    }
+})
+
 
 module.exports = router;
